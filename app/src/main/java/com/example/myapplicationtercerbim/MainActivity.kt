5c983967d2df1d83dbd61769594db4e3fc22c262
rodriquelali <rodriquelali@gmail.com>
2022-08-18 19:21:10 -0400

    funcion suma

--- app/src/main/java/com/example/myapplicationtercerbim/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtercerbim/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.myapplicationtercerbim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+
+        btn.setOnClickListener {
+            val i = Intent(this, proyectjava::class.java)
+            startActivity(i)
+        }
+    }
+
+    //alertas en android studio
+    //Toast
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
